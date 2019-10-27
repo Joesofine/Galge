d2510fcde748e761fa58cc115e707897701f4a27
Joesofine <zainamail@student.dtu.dk>
2019-10-27 17:19:18 +0100

    liste layout

--- app/src/main/java/com/example/galge/Ordliste.java
+++ app/src/main/java/com/example/galge/Ordliste.java
@@ -3,8 +3,11 @@ package com.example.galge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class Ordliste extends AppCompatActivity {
 
@@ -16,11 +19,14 @@ public class Ordliste extends AppCompatActivity {
         setContentView(R.layout.ordliste);
 
         String[] lande = { "Danmark", "Norge", "Sverige"};
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.listeelement, R.id.listeelem_overskrift, lande);
+
         listView = findViewById(R.id.listView1);
-        listView.setAdapter(new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,  android.R.id.text1, lande ));
-
+        listView.setAdapter(adapter);
     }
-
-
+    public void onItemClick(AdapterView<?> liste, View v, int position, long id) {
+        Toast.makeText(this, "Klik på " + position, Toast.LENGTH_SHORT).show();
+    }
 }
+
