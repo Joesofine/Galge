4a6a103033973af413f7e836e7c245d198efe331
Joesofine <zainamail@student.dtu.dk>
2019-10-27 16:02:14 +0100

    I.1.1

--- app/src/androidTest/java/com/example/galge/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/galge/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.galge;
+/** package com.example.galge;
 
 import android.content.Context;
 
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
+**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
