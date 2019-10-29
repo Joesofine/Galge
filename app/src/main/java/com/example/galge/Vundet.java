090fdcce84cc1a28367325adbdb073d667b8c573
Joesofine <zainamail@student.dtu.dk>
2019-10-29 18:29:31 +0100

    Lyd tilføjet til vundet tekst farve ændret

--- app/src/main/java/com/example/galge/Vundet.java
+++ app/src/main/java/com/example/galge/Vundet.java
@@ -3,15 +3,21 @@ package com.example.galge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static android.media.MediaPlayer.*;
+
 public class Vundet extends AppCompatActivity {
 
     TextView forkert;
     Button again, tilbage;
+    MediaPlayer lyd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +27,24 @@ public class Vundet extends AppCompatActivity {
         forkert = findViewById(R.id.forkert);
         again = findViewById(R.id.again);
         tilbage = findViewById(R.id.tilbage);
+        lyd = MediaPlayer.create(this, R.raw.cher);
+        lyd.setVolume(1,1);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int fuldStyrke = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int aktuelStyrke = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        if (aktuelStyrke < fuldStyrke / 5) {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, fuldStyrke / 5, AudioManager.FLAG_SHOW_UI);
+        }
 
         Intent intent = getIntent();
         Bundle fejl = intent.getExtras();
 
         forkert.setText(fejl.getString("forkert")+"!");
+
+        lyd.start();
 
         again.setOnClickListener(new View.OnClickListener() {
             @Override
