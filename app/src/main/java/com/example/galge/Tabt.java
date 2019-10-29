cacaf14ad753cda8b59ae377ed492ad6cc34ace9
Joesofine <zainamail@student.dtu.dk>
2019-10-29 18:32:09 +0100

    Lyd i tabt

--- app/src/main/java/com/example/galge/Tabt.java
+++ app/src/main/java/com/example/galge/Tabt.java
@@ -3,6 +3,8 @@ package com.example.galge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,8 @@ public class Tabt extends AppCompatActivity {
 
     Button again;
     TextView ord;
+    MediaPlayer lyd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,17 @@ public class Tabt extends AppCompatActivity {
 
         again = findViewById(R.id.again);
         ord = findViewById(R.id.ord);
+        lyd = MediaPlayer.create(this, R.raw.boo);
+        lyd.setVolume(1,1);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int fuldStyrke = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int aktuelStyrke = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        if (aktuelStyrke < fuldStyrke / 5) {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, fuldStyrke / 5, AudioManager.FLAG_SHOW_UI);
+        }
 
 
         Intent intent = getIntent();
@@ -28,6 +43,9 @@ public class Tabt extends AppCompatActivity {
 
 
         ord.setText((whatever.getString("ord")));
+
+        lyd.start();
+
 
         again.setOnClickListener(new View.OnClickListener() {
             @Override
